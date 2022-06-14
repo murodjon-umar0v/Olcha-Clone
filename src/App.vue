2c127bbee0bd56f26ff8d5ba9a0fb3eeeb616695
begzod5071 <rafirafikovbegzod5071@gmail.com>
2022-06-14 13:37:06 +0500

    sa

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
-<style>
-</style>
+<script>
+import Header from "./components/Header/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+<style></style>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="header">
-dasd
+  <div class="header">
+   <b-container>
+     <b-row>
+       <b-col>
+         
+       </b-col>
+     </b-row>
+   </b-container>
   </div>
 </template>
 
@@ -10,18 +16,18 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 
-:root{
+:root {
   --tw-gradient-from: #e96443;
   --tw-gradient-to: #904e95;
+
   --tw-gradient-stops: var(--tw-gradient-from),var(--tw-gradient-to,rgba(233,100,67,0));
 
 }
-#header {
-  padding-top: 50px;
-  background-image: linear-gradient(90deg,var(--tw-gradient-stops));
---tw-gradient-stops: var(--tw-gradient-from),var(--tw-gradient-to,rgba(233,100,67,0));
+.header {
+ padding-top: 50px;
+ background-image: linear-gradient(90deg,var(--tw-gradient-stops));
 }
 
 </style>
