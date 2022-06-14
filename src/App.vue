bf52b0299680c06a9299f1cf5523e468a2629525
begzod5071 <rafirafikovbegzod5071@gmail.com>
2022-06-14 12:13:07 +0500

    asda

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-  
+
     <router-view/>
   </div>
 </template>
 
+
+<script>
+export default {
+
+}
+</script>
 <style>
-@import url();
+
 </style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="header"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  padding-top: 50px;
+}
+</style>
+>
