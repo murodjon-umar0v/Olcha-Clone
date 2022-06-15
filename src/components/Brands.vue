4a0b107d5a05d5fe4f9322cb6cdb7aa410b40786
Murod <murodjon.umarovv@gmail.com>
2022-06-15 15:34:58 +0500

    /

--- src/components/Brands.vue
+++ src/components/Brands.vue
@@ -1,16 +1,19 @@
 <template>
- <div class="px-3 mb-5 brands border d-flex justify-between items-center flex-wrap">
-  <div class="w-100 border d-flex justify-between items-center">
+ <div
+  class="px-3 mb-5 brands border d-flex justify-between items-center flex-wrap"
+ >
+  <div class="w-100 border d-flex justify-content-between items-center">
    <span class="h1 fw-normal"> Бренды </span>
-   <router-link to="/brands" class="text-danger border d-flex items-center">
-    Смотреть все
-    <svg xmlns="http://www.w3.org/2000/svg" height="12" viewBox="0 0 320 512">
-     <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-     <path
-      d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"
-     />
-    </svg>
-   </router-link>
+   <span class="d-flex items-center">
+    <router-link to="/brands" class="text-danger border fw-bold pt-3">
+     Смотреть все
+     <svg xmlns="http://www.w3.org/2000/svg" height="12" fill="#DC3545" viewBox="0 0 320 512">
+      <path
+       d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"
+      />
+     </svg>
+    </router-link>
+   </span>
   </div>
   <div class="cards p-2" v-for="(i, idx) in brands" :key="idx">
    <div v-if="idx < 20" class="card px-5 py-3">
