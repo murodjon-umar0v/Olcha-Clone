82b402c1f95e34e6ac70da593a4ab2c8983f74ae
Murod <murodjon.umarovv@gmail.com>
2022-06-15 17:16:46 +0500

    /

--- src/views/BrandView.vue
+++ src/views/BrandView.vue
@@ -3,20 +3,14 @@
   class="px-3 mb-5 brands d-flex justify-content-center items-center flex-wrap"
  >
   <div class="w-100 px-3 d-flex justify-content-between items-center">
-   <span class="h1 fw-normal"> Бренды </span>
-   <span class="d-flex items-center">
-    <router-link to="/brands" class="text-danger fw-bold pt-3">
-     Смотреть все
-     <svg xmlns="http://www.w3.org/2000/svg" height="12" fill="#DC3545" viewBox="0 0 320 512">
-      <path
-       d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"
-      />
-     </svg>
-    </router-link>
-   </span>
+   <span class="h1 fw-normal py-4"> Бренды </span>
   </div>
-  <div class="cards p-2" v-for="(i, idx) in brands" :key="idx">
-   <div v-if="idx < 20" class="card d-flex flex-column items-center justify-content-around py-3">
+  <div
+   class="cards d-flex align-items-stretch p-2"
+   v-for="(i, idx) in brands"
+   :key="idx"
+  >
+  <div v-if="idx < 20" class="card d-flex flex-column items-center justify-content-around py-3">
     <img class="w-50 mx-auto d-block" :src="i.main_image" />
     <span class="text-center pt-2">
      <a :href="i.link" target="_blank">
@@ -25,14 +19,36 @@
     </span>
    </div>
   </div>
+
+  <div class="overflow-auto">
+   <div class="mt-5">
+    <b-pagination
+     v-model="currentPage"
+     hide-goto-end-buttons='false'
+     pills
+     size="lg"
+     :total-rows="rows"
+     @change="getBrands"
+    ></b-pagination>
+   </div>
+  </div>
  </div>
 </template>
 
 <script>
+import { BPagination } from "bootstrap-vue";
+
 export default {
+ name: "Brand",
+ components: {
+  BPagination,
+ },
+
  data() {
   return {
    brands: [],
+   currentPage: 1,
+   rows: 100,
   };
  },
 
@@ -43,7 +59,10 @@ export default {
   getBrands() {
    const request = new XMLHttpRequest();
    // get brand logos of electronics from free api
-   request.open("GET", "https://mobile.olcha.uz/api/v2/manufacturers");
+   request.open(
+    "GET",
+    "https://mobile.olcha.uz/api/v2/manufacturers?page=" + this.currentPage
+   );
    request.send();
    request.onload = () => {
     if (request.status === 200) {
