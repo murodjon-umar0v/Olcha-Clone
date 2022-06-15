ada3277d491bdf50b2f9aa9bceec1a5d4f3d57f8
Murod <murodjon.umarovv@gmail.com>
2022-06-15 12:49:34 +0500

    /

--- src/components/ReklamaCard.vue
+++ src/components/ReklamaCard.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="row border my-3 mx-3">
-        <div class="col-12 col-lg-4 border">
-            <div class="container border">
+    <div class="row my-5 py-4 mx-3">
+        <div class="col-12 col-lg-4">
+            <div class="container">
                 <img class="img-fluid w-100 mx-auto rounded" src="https://olcha.uz/image/380x132/discount/KK/KK/ci/1637741582.jpg" alt="error">
             </div>
         </div>
-        <div class="col-12 col-lg-4 border">
-            <div class="container border">
+        <div class="col-12 col-lg-4">
+            <div class="container">
                 <img class="img-fluid w-100 mx-auto rounded" src="https://olcha.uz/image/380x132/discount/KK/Kh/L2/1652169577.jpg" alt="error">
             </div>
         </div>
-        <div class="col-12 col-lg-4 border">
-            <div class="container border">
+        <div class="col-12 col-lg-4">
+            <div class="container">
                 <img class="img-fluid w-100 mx-auto rounded" src="https://olcha.uz/image/380x132/discount/KK/KK/c1/1635851437.png" alt="error">
             </div>
         </div>
