474b0df45700ce8957a3efcdf5ba51fe3322f062
Murod <murodjon.umarovv@gmail.com>
2022-06-16 14:38:29 +0500

    /

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,27 +1,59 @@
 <template>
-  <router-link to="/product/view">
-    <div class="card-box">
-      <div class="d-flex flex-column position-relative">
-        <img src="../assets/phone.jpeg" alt="phone" />
-       <button class="position-absolute border-0 bg-transparent top-0 end-0">
-         <svg width="22" height="22" fill="red" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z"/></svg>
-       </button>
-       <button class="position-absolute border-0 bg-transparent end-0 mt-4">
-        <img class="mt-3" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGgKICAgIGZpbGw9IiNGOTExNTUiCiAgICBkPSJNMTQuOCA2LjRsLTUtNS4xYy0uMi0uMi0uNC0uMy0uNy0uMy0uMiAwLS41LjEtLjcuM2wtLjEuMWMtLjMuNC0uMiAxIC4xIDEuM0wxMS42IDZIOS4xQzQuNiA2IDEgOS42IDEgMTQuMWMwIC41LjQuOS45LjlzLjktLjQuOS0uOXYtLjJjLjEtMy40IDIuOC02IDYuMi02aDIuNmwtMy4yIDMuM2MtLjQuNC0uNCAxIDAgMS40LjIuMi40LjMuNy4zLjMgMCAuNS0uMS43LS4zbDUtNS4xYy4zLS4zLjMtLjggMC0xLjF6IgogIC8+Cjwvc3ZnPgo=" alt="target">
-       </button>
-      </div>
-      <p class="mb-1 text-dark fs-5 mt-3">Смартфон</p>
-      <span class="text-dark fs-4 fw-bolder">3 102 000 сум</span>
-      <span class="text-white d-block bg-danger rounded-pill ps-2 mb-3">от 321 000 сум/месяц</span>
-      <span class="d-block text-dark mb-3">Смартфон Samsung Galaxy A32 6 GB 128GB Черный</span>
-     <div class="d-flex align-items-center justify-content-between">
-       <button class="border rounded-pill btn px-5 bg-body border-dark shadow "><span class="px-2">Купить</span></button>
-<button class="border-0 bg-transparent">
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><rect x="3.25" y="15" width="2.5" height="7" rx="1.25"></rect> <rect x="8.25" y="3" width="2.5" height="19" rx="1.25"></rect> <rect x="13.25" y="11" width="2.5" height="11" rx="1.25"></rect> <rect x="18.25" y="7" width="2.5" height="15" rx="1.25"></rect></svg>
-</button>
-     </div>
-    </div>
-  </router-link>
+ <router-link to="/product/view">
+  <div class="card-box">
+   <div class="d-flex flex-column position-relative">
+    <img src="../assets/phone.jpeg" alt="phone" />
+    <button class="position-absolute border-0 bg-transparent top-0 end-0">
+     <svg
+      width="22"
+      height="22"
+      fill="red"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+     >
+      <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+      <path
+       d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z"
+      />
+     </svg>
+    </button>
+    <button class="position-absolute border-0 bg-transparent end-0 mt-4">
+     <img
+      class="mt-3"
+      src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGgKICAgIGZpbGw9IiNGOTExNTUiCiAgICBkPSJNMTQuOCA2LjRsLTUtNS4xYy0uMi0uMi0uNC0uMy0uNy0uMy0uMiAwLS41LjEtLjcuM2wtLjEuMWMtLjMuNC0uMiAxIC4xIDEuM0wxMS42IDZIOS4xQzQuNiA2IDEgOS42IDEgMTQuMWMwIC41LjQuOS45LjlzLjktLjQuOS0uOXYtLjJjLjEtMy40IDIuOC02IDYuMi02aDIuNmwtMy4yIDMuM2MtLjQuNC0uNCAxIDAgMS40LjIuMi40LjMuNy4zLjMgMCAuNS0uMS43LS4zbDUtNS4xYy4zLS4zLjMtLjggMC0xLjF6IgogIC8+Cjwvc3ZnPgo="
+      alt="target"
+     />
+    </button>
+   </div>
+   <p class="mb-1 text-dark fs-5 mt-3">Смартфон</p>
+   <span class="text-dark fs-4 fw-bolder">3 102 000 сум</span>
+   <span class="text-white d-block bg-danger rounded-pill ps-2 mb-3"
+    >от 321 000 сум/месяц</span
+   >
+   <span class="d-block text-dark mb-3"
+    >Смартфон Samsung Galaxy A32 6 GB 128GB Черный</span
+   >
+   <div class="d-flex align-items-center justify-content-between">
+    <button class="border rounded-pill btn px-5 bg-body border-dark shadow">
+     <span class="px-2">Купить</span>
+    </button>
+    <button class="border-0 bg-transparent">
+     <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="black"
+      xmlns="http://www.w3.org/2000/svg"
+     >
+      <rect x="3.25" y="15" width="2.5" height="7" rx="1.25"></rect>
+      <rect x="8.25" y="3" width="2.5" height="19" rx="1.25"></rect>
+      <rect x="13.25" y="11" width="2.5" height="11" rx="1.25"></rect>
+      <rect x="18.25" y="7" width="2.5" height="15" rx="1.25"></rect>
+     </svg>
+    </button>
+   </div>
+  </div>
+ </router-link>
 </template>
 
 <script>
