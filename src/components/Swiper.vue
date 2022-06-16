c99b1272198a332063c5072f5a3e7841e2540177
begzod5071 <rafirafikovbegzod5071@gmail.com>
2022-06-16 16:56:35 +0500

    min

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,23 +1,19 @@
 <template>
- <div class="px-3 my-4 catalog text-center container">
-  <swiper
-   :loop="false"
-   :pagination="true"
-   :navigation="true"
-  >
-   <swiper-slide v-for="n in category" :key="n">
-    <img
-     :src="n.main_image"
-     class="img-fluid d-block mx-auto"
-     blank="true"
-     style="width: 60px; height: 50px"
-    />
-    <span class="d-flex justify-content-center">
-     {{ n.name_ru }}
-    </span>
-   </swiper-slide>
-  </swiper>
- </div>
+  <div class="px-3 my-4 catalog text-center container">
+    <swiper :options="swiperLotOptions2">
+      <swiper-slide v-for="n in category" :key="n">
+        <img
+          :src="n.main_image"
+          class=""
+          blank="true"
+          style="width: 60px; height: 50px"
+        />
+        <span class="d-flex justify-content-center">
+          {{ n.name_ru }}
+        </span>
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script>
@@ -31,71 +27,77 @@ import "swiper/swiper-bundle.css";
 SwiperCore.use([Navigation, Pagination]);
 
 export default {
- components: {
-  Swiper,
-  SwiperSlide,
- },
- data() {
-  return {
-   category: [],
-   breakpoints: {
-    300: {
-     slidesPerView: 3,
-     spaceBetweenSlides: 30,
-    },
-    500: {
-     slidesPerView: 4,
-     spaceBetweenSlides: 30,
-    },
-    800: {
-     slidesPerView: 6,
-     spaceBetweenSlides: 30,
-    },
-    999: {
-     slidesPerView: 7,
-     spaceBetweenSlides: 30,
-    },
-    1100: {
-     slidesPerView: 9,
-     spaceBetweenSlides: 30,
-    },
-   },
-  };
- },
- mounted() {
-  this.getCategory();
- },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      category: [],
+      swiperLotOptions2: {
+        slidesPerView: 2,
+        loop: false,
+        pagination: true,
+        navigation: true,
+        autoplay: true,
+        spaceBetween: 17,
+        centeredSlides: true,
+        breakpoints: {
+          300: {
+            slidesPerView: 8,
+            spaceBetweenSlides: 30,
+          },
+          500: {
+            slidesPerView: 5,
+            spaceBetweenSlides: 30,
+          },
+          800: {
+            slidesPerView: 5,
+            spaceBetweenSlides: 30,
+          },
+          999: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 30,
+          },
+          1100: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 30,
+          },
+        },
+      },
+    };
+  },
+  created() {
+    this.getCategory();
+  },
 
- methods: {
-  getCategory() {
-   fetch("https://mobile.olcha.uz/api/v2/categories")
-    .then((response) => response.json())
-    .then((data) => {
-     this.category = data.data.categories;
-     console.log(data.data.categories);
-    });
+  methods: {
+    getCategory() {
+      fetch("https://mobile.olcha.uz/api/v2/categories")
+        .then((response) => response.json())
+        .then((data) => {
+          this.category = data.data.categories;
+          console.log(data.data.categories);
+        });
+    },
   },
-  getImageUrl(imageId) {
-   return `https://picsum.photos/600/400/?image=${imageId}`;
-  },
- },
 };
 </script>
 
 <style scoped>
 .img-fluid {
- max-width: 100%;
- height: auto;
+  max-width: 100%;
+  height: auto;
 }
 .w-100 {
- width: 100%;
+  width: 100%;
 }
 .ml-auto,
 .mx-auto {
- margin-left: auto;
+  margin-left: auto;
 }
 .mr-auto,
 .mx-auto {
- margin-right: auto;
+  margin-right: auto;
 }
 </style>
